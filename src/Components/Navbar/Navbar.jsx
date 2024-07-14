a876e29a626febbaba0fb95aead43e96522e564c
bestdevelope <leabouillot7@gmail.com>
2024-07-14 19:02:13 +0200

    nave bar css

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,24 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
+import { useSearchParams } from "react-router-dom";
 
 const Navbar = () => {
+  //applique <hr/> si on click menu
+  const [menu, setMenu] = useState("shop");
   return (
     <div className="navbar">
-      <div>
-        <img src={logo} alt="logo" />
+      <div className="nav-logo">
+        <img src={logo} alt="logo" width={70} />
+        <p>ShopFul</p>
       </div>
 
       <ul className="nav-menu">
-        <li>Shop</li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
+        <li
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          Shop{menu === "shop" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("women");
+          }}
+        >
+          Women{menu === "women" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("men");
+          }}
+        >
+          Men{menu === "men" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("kids");
+          }}
+        >
+          Kids{menu === "kids" ? <hr /> : <></>}
+        </li>
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>
         <img src={cart_icon} alt="" />
+        <div className="nav-cart-count">0</div>
       </div>
     </div>
   );
